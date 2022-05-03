83ab19ef8df736499b58ee01c30db564bc8f1684
Loghorrean <Loghorrean>
2022-05-03 10:44:53 +0300

    Returned cors headers

--- src/middleware/setCorsHeaders.ts
+++ src/middleware/setCorsHeaders.ts
@@ -2,6 +2,6 @@ import {Request, Response} from "express";
 import appConfig from "../config/app";
 
 export function setCorsHeaders(req: Request, res: Response, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Origin", appConfig.clientBaseUrl)
     next();
 }
